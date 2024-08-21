a10d585bbdcc1a79fec6f78e0c98cac31839311b
MohammadMoktabaRoshani <mohammadmojtabaroshani@outlook.com>
2024-08-21 23:13:57 +0330

    fix[app], icons & sharing message

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -14,6 +14,15 @@ export const siteConfig: SiteConfig = {
 	description: "MohammdMojtaba Roshani (mmRoshani)'s Knowledge Base", // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
-	shareMessage: 'Share', // Message to share a post on social media
+	shareMessage: `————————————————
+	\n
+	WRITE_YOUR_CUSTOM_MESSAGE_HERE
+	\n
+	 ————————————————
+	- Blog: https://mmroshani.github.io
+	- GitHub: https://github.com/mmRoshani
+	- Linkedin: https://linkedin.com/in/mmRoshani
+	- Telegram Channel: https://t.me/mmRoshani_kb
+	- Telegram Account: https://t.me/real_mmRoshani`,
 	paginationSize: 6 // Number of posts per page
 }
